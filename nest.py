3a2ba361ea340bbf4d634a36dae26463f3d1be4b
Sameer Parekh Brenn <sameer@creativedestruction.com>
2014-12-06 14:27:26 -0700

    force to use tlsv1

--- nest.py
+++ nest.py
@@ -2,6 +2,16 @@ __author__ = 'sameer'
 
 import requests
 from dateutil import parser
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.poolmanager import PoolManager
+import ssl
+
+class MyAdapter(HTTPAdapter):
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = PoolManager(num_pools=connections,
+                                       maxsize=maxsize,
+                                       block=block,
+                                       ssl_version=ssl.PROTOCOL_TLSv1)
 
 class Nest(object):
     user_agent = 'Nest/2.1.3 CFNetwork/548.0.4'
@@ -12,6 +22,8 @@ class Nest(object):
     def __init__(self, username, password):
         self.username = username
         self.password = password
+        self.session = requests.Session()
+        self.session.mount("https://", MyAdapter())
 
     @property
     def headers(self):
@@ -24,7 +36,7 @@ class Nest(object):
                 'Authorization': 'Basic %s' % self.access_token}
 
     def login(self):
-        r = requests.post(self.login_url, headers=self.headers,
+        r = self.session.post(self.login_url, headers=self.headers,
                           data={'username': self.username,
                                                   'password': self.password})
         json = r.json()
@@ -32,10 +44,10 @@ class Nest(object):
         self.access_token = json['access_token']
         self.userid = json['userid']
         self.user = json['user']
+        self.cookies = r.cookies
         self.cache_expiration = parser.parse(json['expires_in'])
 
-    def request(self, url, method=requests.get, **kwargs):
-        print url
+    def request(self, url, method=None, **kwargs):
         headers = self.headers.copy()
         headers.update(self.auth_headers)
 
@@ -43,17 +55,23 @@ class Nest(object):
             headers.update(kwargs['headers'])
             del kwargs['headers']
 
-        r = method(url, headers=headers, **kwargs)
+        if method is None:
+            method = self.session.get
+
+        r = method(url, headers=headers, cookies=self.cookies, **kwargs)
         return r
 
     def get_status(self):
         r = self.request(self.transport_url + '/v3/mobile/' + self.user)
-        pass
+        return r.json()
 
+    def get_weather(self, postal_code):
+        r = self.request("https://home.nest.com/api/0.1/weather/forecast/%s" % postal_code)
+        return r.json()
 
 if __name__ == "__main__":
     nest = Nest('nest.com@spam.creativedestruction.com', 'phom5vysh9phop')
     nest.login()
-    nest.get_status()
+    print nest.get_status()
 
 
